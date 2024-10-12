8c0662d5526d6f3e26965170083ca77400434543
RukiTamTam <wcurimatmat24@gmail.com>
2024-10-12 09:52:55 +0800

    added markup for home page

---
+++ resources/views/home.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    @vite('resources/css/app.css')
+</head>
+
+<body>
+    <div class="container">
+        <x-nav />
+
+        <main>
+            <div class="hero-section">The Hero</div>
+            <div class="features-section">The Features</div>
+            <div class="about-section">The About</div>
+            <div class="faq-section">The FAQ</div>
+            <div class="contact-section">The Contact</div>
+        </main>
+
+        <footer>
+            The footer
+        </footer>
+    </div>
+</body>
+
+</html>
