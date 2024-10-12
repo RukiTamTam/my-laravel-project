12222cdf1923351dbb9e00c451fa2f2c70b02192
RukiTamTam <wcurimatmat24@gmail.com>
2024-10-12 09:31:06 +0800

    markup for navigation bar

---
+++ resources/views/components/nav.blade.php
@@ -0,0 +1,23 @@
+<nav class="Nav">
+    <div class="Nav__logo-wrapper">
+        <a href="" class="Nav__link">
+            TheLogo
+        </a>
+    </div>
+
+    <ul class="Nav__link-wrapper">
+        <div class="container">
+            <li class="Nav__link-item"><a href="" class="Nav__link">Home</a></li>
+            <li class="Nav__link-item"><a href="" class="Nav__link">Products</a></li>
+            <li class="Nav__link-item"><a href="" class="Nav__link">About</a></li>
+            <li class="Nav__link-item"><a href="" class="Nav__link">Contact</a></li>
+            <li class="Nav__link-item"><a href="" class="Nav__link">Blog</a></li>
+        </div>
+    </ul>
+
+    <div class="Nav__actions">
+        <button class="Nav__action-1">Search</button>
+        <button class="Nav__action-2">Cart</button>
+        <button class="Nav__action-3">Login</button>
+    </div>
+</nav>
